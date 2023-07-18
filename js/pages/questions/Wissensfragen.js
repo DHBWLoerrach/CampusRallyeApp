f4503d53a660bb339eb371eadd1ad6457030fdb8
Erik Behrends <behrends@users.noreply.github.com>
2023-07-18 19:36:32 +0200

    Typos

--- js/pages/questions/Wissensfragen.js
+++ js/pages/questions/Wissensfragen.js
@@ -1,89 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet, ScrollView } from 'react-native';
-import {useSharedStates} from '../sharedStates'
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import { useSharedStates } from '../sharedStates';
 import { supabase } from '../../../supabase';
 
-export default function Wissensfragen () {
+export default function Wissensfragen() {
   // import shared States
-  const {fragen, setFragen} = useSharedStates();
-  const {aktuelleFrage, setAktuelleFrage} = useSharedStates();
-  const {points, setPoints} = useSharedStates();
-  const [answer, setAnswer] = useState("");
+  const { fragen, setFragen } = useSharedStates();
+  const { aktuelleFrage, setAktuelleFrage } = useSharedStates();
+  const { points, setPoints } = useSharedStates();
+  const [answer, setAnswer] = useState('');
   const [correctAnswer, setCorrectAnswer] = useState('');
-  const [confirmedAnswer, setConfirmedAnswer] = useState("");
-  const [answered, setAnswered] = useState(false)
+  const [confirmedAnswer, setConfirmedAnswer] = useState('');
+  const [answered, setAnswered] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const { data: answer, error } = await supabase
-      .from('Wissensfragen')
-      .select('Antwort, Punkte') 
-      .eq('fragen_id', fragen[aktuelleFrage].fragen_id);
-      setCorrectAnswer(answer)
+        .from('Wissensfragen')
+        .select('Antwort, Punkte')
+        .eq('fragen_id', fragen[aktuelleFrage].fragen_id);
+      setCorrectAnswer(answer);
     };
     fetchData();
   }, [!answered]);
 
   const handleNext = () => {
-    if (answer.trim() === correctAnswer[0].Antwort){
-      setPoints(points+correctAnswer[0].Punkte);
+    if (answer.trim() === correctAnswer[0].Antwort) {
+      setPoints(points + correctAnswer[0].Punkte);
     }
-    setAktuelleFrage(aktuelleFrage+1);   
-    setAnswer(""); 
+    setAktuelleFrage(aktuelleFrage + 1);
+    setAnswer('');
     setAnswered(false);
   };
 
   const handleAnswerSubmit = () => {
     setAnswered(true);
     if (answer.trim() === '') {
-      Alert.alert('Fehler', 'Bitte geben Sie eine Antwort ein.');
+      Alert.alert('Fehler', 'Bitte gebe eine Antwort ein.');
       return;
     }
 
     Alert.alert(
-      "Sicherheitsfrage",
+      'Sicherheitsfrage',
       `Bist du sicher, dass "${answer}" deine endgültige Antwort ist?`,
       [
         {
-          text: "Abbrechen",
-          style: "cancel",
+          text: 'Abbrechen',
+          style: 'cancel',
         },
         {
-          text: "Ja, Antwort bestätigen",
+          text: 'Ja, Antwort bestätigen',
           onPress: () => handleNext(),
         },
-      ],
+      ]
     );
   };
-  
+
   return (
     <ScrollView>
-    <View style={styles.container}>
-      <Text style={styles.question}>{fragen[aktuelleFrage].frage}</Text>
-      <Text style={styles.inputLabel}>Antwort:</Text>
-      <TextInput
-        style={styles.input}
-        value={answer}
-        onChangeText={setAnswer}
-        placeholder="Gib hier deine Antwort ein"
-      />
-      <Button
-        title="Antwort Absenden"
-        onPress={handleAnswerSubmit}
-        disabled={!answer}
-      />
-      
-      {confirmedAnswer ? (
-        <View style={styles.answerContainer}>
-          <Text style={styles.answerLabel}>Bestätigte Antwort:</Text>
-          <Text style={styles.answer}>{confirmedAnswer}</Text>
-        </View>
-      ) : null}
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.question}>
+          {fragen[aktuelleFrage].frage}
+        </Text>
+        <Text style={styles.inputLabel}>Antwort:</Text>
+        <TextInput
+          style={styles.input}
+          value={answer}
+          onChangeText={setAnswer}
+          placeholder="Gib hier deine Antwort ein"
+        />
+        <Button
+          title="Antwort senden"
+          onPress={handleAnswerSubmit}
+          disabled={!answer}
+        />
+
+        {confirmedAnswer ? (
+          <View style={styles.answerContainer}>
+            <Text style={styles.answerLabel}>
+              Bestätigte Antwort:
+            </Text>
+            <Text style={styles.answer}>{confirmedAnswer}</Text>
+          </View>
+        ) : null}
+      </View>
     </ScrollView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
