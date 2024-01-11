a588d9aeedd5de51d0287706600cb28da49a18c1
Erik Behrends <behrends@users.noreply.github.com>
2024-01-11 16:18:15 +0100

    Fix group selection

--- screens/GroupScreen.jsx
+++ screens/GroupScreen.jsx
@@ -51,13 +51,11 @@ export default function GroupScreen() {
   if (!useRallye) {
     return (
       <View style={styles.container}>
-        <Text style={styles.modalText}>
+        <Text style={[styles.text, { marginBottom: 20 }]}>
           Es ist aktuell keine Rallye aktiv.
         </Text>
         <UIButton size="small" onClick={() => setEnabled(false)}>
-          <Text style={{ textAlign: 'center' }}>
-            Zurück zur Anmeldung
-          </Text>
+          Zurück zur Anmeldung
         </UIButton>
       </View>
     );
@@ -76,7 +74,14 @@ export default function GroupScreen() {
             },
           ]}
         >
-          <Text style={styles.sectionTitle}>Gruppe {index + 1}</Text>
+          <Text
+            style={[
+              styles.sectionTitle,
+              { color: item.id === group ? Colors.dhbwRed : 'black' },
+            ]}
+          >
+            Gruppe {index + 1}
+          </Text>
           <View style={styles.row}>
             <Text style={styles.label}>Name der Gruppe:</Text>
             <Text style={styles.value}>{item.name}</Text>
@@ -125,66 +130,22 @@ export default function GroupScreen() {
 }
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    backgroundColor: Colors.dhbwRed,
-    margin: 6,
-    borderRadius: 5,
-    borderWidth: 2,
-    flex: 1,
-    alignSelf: 'center',
-  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
   },
-  modalContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-    width: '80%',
-    alignItems: 'center',
-  },
-  modalText: {
+  text: {
     fontSize: 20,
-    marginBottom: 20,
+    color: 'grey',
     textAlign: 'center',
-  },
-  inputLabel: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: Colors.dhbwGray,
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-  answerContainer: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  answerLabel: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  answer: {
-    fontSize: 16,
   },
   section: {
     marginTop: 20,
     backgroundColor: '#fff',
     borderRadius: 10,
+    borderWidth: 1,
     padding: 20,
     width: '100%',
   },
