3261ffb78d71c05bd7c5c3a1bcc0327ab659f0c4
Erik Behrends <behrends@users.noreply.github.com>
2024-07-01 13:24:44 +0200

    Format with Prettier

--- screens/RallyeScreen.jsx
+++ screens/RallyeScreen.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, RefreshControl, ScrollView, } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  RefreshControl,
+  ScrollView,
+} from 'react-native';
 import { supabase } from '../utils/Supabase';
 import SkillQuestions from './questions/SkillQuestions';
 import UploadQuestions from './questions/UploadQuestions';
@@ -23,197 +29,261 @@ export default function RallyeScreen() {
     rallye,
     setRallye,
     setPoints,
-    remainingTime
+    remainingTime,
   } = useSharedStates();
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
 
-  const reloadRallye = async ()=>{
+  const reloadRallye = async () => {
     const { data: rallye } = await supabase
-          .from('rallye')
-          .select('*')
-          .eq('is_active_rallye', true);
-          setRallye(rallye[0])
-  }
+      .from('rallye')
+      .select('*')
+      .eq('is_active_rallye', true);
+    setRallye(rallye[0]);
+  };
 
   const onRefresh = React.useCallback(() => {
-    if(useRallye){
+    if (useRallye) {
       setRefreshing(true);
       reloadRallye().then(() => setRefreshing(false));
     }
   }, []);
 
-    useEffect(() => {
-      if(!useRallye){return}
-        if (rallye.status == "running") {
-          if (group !== null) {
-            const fetchData = async () => {
-              let group_id = group;
-              let { data, error } = await supabase.rpc('get_questions', {
-                group_id,
-              });
-              if (data) {
-                temp = data.filter(item => item.question_type !== 'multiple_choice');
-                multiple_choice_parent = data.filter(item => item.question_type === 'multiple_choice' && item.parent_id === null);
-                multiple_choice_child = data.filter(item => item.question_type === 'multiple_choice' && item.parent_id !== null);
-  
-                for (let index = 0; index < multiple_choice_parent.length; index++) {
-                  const element = multiple_choice_parent[index];
-                  childs = multiple_choice_child.filter(item => item.parent_id === element.id)
-                  const childAnswers = childs.map(child => child.answer);
-                  element.multiple_answer = childAnswers;
-                }
-                data = temp.concat(multiple_choice_parent)
-                for (let i = data.length - 1; i > 0; i--) {
-                  const j = Math.floor(Math.random() * (i + 1));
-                  [data[i], data[j]] = [data[j], data[i]];
-                }
-              }
-              setQuestions(data);
-              setLoading(false);
-            };
-  
-            fetchData();
-          }
-        }
-
-    }, [rallye, group]);
-
-    useEffect(() => {
-      if(!useRallye){return}
-      if (rallye.status == "running") {
-        if (currentQuestion === null) {
-          const fetchData = async () => {
-            let group_id_param = group;
-            let { data, error } = await supabase.rpc(
-              'get_points',
-              {
-                group_id_param,
-              }
-            );
-
-            setPoints(data);
-          };
-          fetchData();
-        } else if (currentQuestion === questions.length) {
-          const fetchData = async () => {
-            let group_id_param = group;
-            let { data, error } = await supabase.rpc(
-              'get_points',
-              {
-                group_id_param,
-              }
-            );
-
-            setPoints(data);
-          };
-          fetchData();
-        }
-      }
-    }, [rallye, currentQuestion]);
-
-    useEffect(() => {
-      if(useRallye){return}
+  useEffect(() => {
+    if (!useRallye) {
+      return;
+    }
+    if (rallye.status == 'running') {
+      if (group !== null) {
         const fetchData = async () => {
-          let { data } = await supabase
-            .from('question')
-            .select('*')
-            .eq('enabled', true)
-            .neq('question_type', 'upload');
-          temp = data.filter(item => item.question_type !== 'multiple_choice');
-          multiple_choice_parent = data.filter(item => item.question_type === 'multiple_choice' && item.parent_id === null);
-          multiple_choice_child = data.filter(item => item.question_type === 'multiple_choice' && item.parent_id !== null);
-  
-          for (let index = 0; index < multiple_choice_parent.length; index++) {
-            const element = multiple_choice_parent[index];
-            childs = multiple_choice_child.filter(item => item.parent_id === element.id)
-            const childAnswers = childs.map(child => child.answer);
-            element.multiple_answer = childAnswers;
-          }
-  
-          data = temp.concat(multiple_choice_parent)
-          for (let i = data.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [data[i], data[j]] = [data[j], data[i]];
+          let group_id = group;
+          let { data, error } = await supabase.rpc('get_questions', {
+            group_id,
+          });
+          if (data) {
+            temp = data.filter(
+              (item) => item.question_type !== 'multiple_choice'
+            );
+            multiple_choice_parent = data.filter(
+              (item) =>
+                item.question_type === 'multiple_choice' &&
+                item.parent_id === null
+            );
+            multiple_choice_child = data.filter(
+              (item) =>
+                item.question_type === 'multiple_choice' &&
+                item.parent_id !== null
+            );
+
+            for (
+              let index = 0;
+              index < multiple_choice_parent.length;
+              index++
+            ) {
+              const element = multiple_choice_parent[index];
+              childs = multiple_choice_child.filter(
+                (item) => item.parent_id === element.id
+              );
+              const childAnswers = childs.map(
+                (child) => child.answer
+              );
+              element.multiple_answer = childAnswers;
+            }
+            data = temp.concat(multiple_choice_parent);
+            for (let i = data.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [data[i], data[j]] = [data[j], data[i]];
+            }
           }
           setQuestions(data);
           setLoading(false);
         };
+
         fetchData();
-      
-    }, []);
-  
+      }
+    }
+  }, [rallye, group]);
+
+  useEffect(() => {
+    if (!useRallye) {
+      return;
+    }
+    if (rallye.status == 'running') {
+      if (currentQuestion === null) {
+        const fetchData = async () => {
+          let group_id_param = group;
+          let { data, error } = await supabase.rpc('get_points', {
+            group_id_param,
+          });
+
+          setPoints(data);
+        };
+        fetchData();
+      } else if (currentQuestion === questions.length) {
+        const fetchData = async () => {
+          let group_id_param = group;
+          let { data, error } = await supabase.rpc('get_points', {
+            group_id_param,
+          });
+
+          setPoints(data);
+        };
+        fetchData();
+      }
+    }
+  }, [rallye, currentQuestion]);
+
+  useEffect(() => {
+    if (useRallye) {
+      return;
+    }
+    const fetchData = async () => {
+      let { data } = await supabase
+        .from('question')
+        .select('*')
+        .eq('enabled', true)
+        .neq('question_type', 'upload');
+      temp = data.filter(
+        (item) => item.question_type !== 'multiple_choice'
+      );
+      multiple_choice_parent = data.filter(
+        (item) =>
+          item.question_type === 'multiple_choice' &&
+          item.parent_id === null
+      );
+      multiple_choice_child = data.filter(
+        (item) =>
+          item.question_type === 'multiple_choice' &&
+          item.parent_id !== null
+      );
+
+      for (
+        let index = 0;
+        index < multiple_choice_parent.length;
+        index++
+      ) {
+        const element = multiple_choice_parent[index];
+        childs = multiple_choice_child.filter(
+          (item) => item.parent_id === element.id
+        );
+        const childAnswers = childs.map((child) => child.answer);
+        element.multiple_answer = childAnswers;
+      }
+
+      data = temp.concat(multiple_choice_parent);
+      for (let i = data.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [data[i], data[j]] = [data[j], data[i]];
+      }
+      setQuestions(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
   let content;
-  if(useRallye){
-    if (!loading && rallye.status === "running" && remainingTime >= 0&&questions !== null && currentQuestion < questions.length) {
+  if (useRallye) {
+    if (
+      !loading &&
+      rallye.status === 'running' &&
+      remainingTime >= 0 &&
+      questions !== null &&
+      currentQuestion < questions.length
+    ) {
       if (questions[currentQuestion].question_type === 'knowledge') {
         content = <SkillQuestions />;
-      } else if (questions[currentQuestion].question_type === 'upload') {
+      } else if (
+        questions[currentQuestion].question_type === 'upload'
+      ) {
         content = <UploadQuestions />;
       } else if (questions[currentQuestion].question_type === 'qr') {
         content = <QRCodeQuestions />;
-      } else if (questions[currentQuestion].question_type === 'multiple_choice') {
+      } else if (
+        questions[currentQuestion].question_type === 'multiple_choice'
+      ) {
         content = <MultipleChoiceQuestions />;
-      } else if (questions[currentQuestion].question_type === 'picture') {
+      } else if (
+        questions[currentQuestion].question_type === 'picture'
+      ) {
         content = <ImageQuestions />;
       }
-    } else if (useRallye&&rallye.status === "post_processing"){
+    } else if (useRallye && rallye.status === 'post_processing') {
       content = (
         <ScrollView
           contentContainerStyle={styles.refreshContainer}
           refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
           }
         >
           <VotingScreen />
         </ScrollView>
       );
-    }
-    else if (!loading&&rallye.status=="running"&&remainingTime>=0) {
+    } else if (
+      !loading &&
+      rallye.status == 'running' &&
+      remainingTime >= 0
+    ) {
       content = (
         <ScrollView
-        contentContainerStyle={styles.refreshContainer}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      >
-        <View>
-          <Text style={styles.endText}>
-          Ihr habt alle Fragen beantwortet, Glückwunsch!
-          </Text>
-          <Text style={styles.endText}>
-          Wartet bis die Rallye beendet wird, um das Ergebnis zu sehen.</Text>
-          <Text style={styles.endText}>
-          Eure erreichte Punktzahl: {points}
-          </Text>
-          <Text style={styles.endText}>
-          Geht zu eurem vereinbarten Treffpunkt.
-          </Text>
-        </View>
-      </ScrollView>
+          contentContainerStyle={styles.refreshContainer}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
+          }
+        >
+          <View>
+            <Text style={styles.endText}>
+              Ihr habt alle Fragen beantwortet, Glückwunsch!
+            </Text>
+            <Text style={styles.endText}>
+              Wartet bis die Rallye beendet wird, um das Ergebnis zu
+              sehen.
+            </Text>
+            <Text style={styles.endText}>
+              Eure erreichte Punktzahl: {points}
+            </Text>
+            <Text style={styles.endText}>
+              Geht zu eurem vereinbarten Treffpunkt.
+            </Text>
+          </View>
+        </ScrollView>
       );
-    } else if(!loading&&rallye.status=="running"&&remainingTime<0){
+    } else if (
+      !loading &&
+      rallye.status == 'running' &&
+      remainingTime < 0
+    ) {
       content = (
         <ScrollView
-        contentContainerStyle={styles.refreshContainer}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      >
-        <View>
-          <Text style={styles.endText}>
-          Die Zeit für die Rallye ist abgelaufen. Geht zum Treffpunkt.
-          </Text>
-          <Text style={styles.endText}>
-          Wartet bis die Rallye beendet wird um das Ergebnis zu sehen.</Text>
-          <Text style={styles.endText}>
-          Eure erreichte Punktzahl: {points}
-          </Text>
-        </View>
-      </ScrollView>
+          contentContainerStyle={styles.refreshContainer}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
+          }
+        >
+          <View>
+            <Text style={styles.endText}>
+              Die Zeit für die Rallye ist abgelaufen. Geht zum
+              Treffpunkt.
+            </Text>
+            <Text style={styles.endText}>
+              Wartet bis die Rallye beendet wird um das Ergebnis zu
+              sehen.
+            </Text>
+            <Text style={styles.endText}>
+              Eure erreichte Punktzahl: {points}
+            </Text>
+          </View>
+        </ScrollView>
       );
-    } else if(useRallye&&loading && group === null) {
+    } else if (useRallye && loading && group === null) {
       content = (
         <View>
           <Text style={styles.groupSelectionText}>
@@ -221,55 +291,56 @@ export default function RallyeScreen() {
           </Text>
         </View>
       );
-    } else if(useRallye&&rallye.status == "preparation"){
+    } else if (useRallye && rallye.status == 'preparation') {
       content = (
         <ScrollView
-    contentContainerStyle={styles.specialRefreshConatiner}
-    refreshControl={
-      <RefreshControl
-        refreshing={refreshing}
-        onRefresh={onRefresh}
-      />
+          contentContainerStyle={styles.specialRefreshConatiner}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
+          }
+        >
+          <Text style={styles.groupSelectionText}>
+            Die Rallye hat noch nicht angefangen.
+          </Text>
+        </ScrollView>
+      );
+    } else if (rallye.status == 'ended') {
+      content = Scoreboard();
     }
-  >
-    <Text style={styles.groupSelectionText}>
-      Die Rallye hat noch nicht angefangen. 
-    </Text>
-  </ScrollView>
-      )
-    }
-  
-    else if(rallye.status == "ended"){
-        content = (
-            Scoreboard()
-        )
-    }
-  }else{
-    if (!loading && questions !== null && currentQuestion !== questions.length) {
+  } else {
+    if (
+      !loading &&
+      questions !== null &&
+      currentQuestion !== questions.length
+    ) {
       if (questions[currentQuestion].question_type === 'knowledge') {
         content = <SkillQuestions />;
       } else if (questions[currentQuestion].question_type === 'qr') {
         content = <QRCodeQuestions />;
-      } else if (questions[currentQuestion].question_type === 'multiple_choice') {
+      } else if (
+        questions[currentQuestion].question_type === 'multiple_choice'
+      ) {
         content = <MultipleChoiceQuestions />;
-      } else if (questions[currentQuestion].question_type === 'picture') {
+      } else if (
+        questions[currentQuestion].question_type === 'picture'
+      ) {
         content = <ImageQuestions />;
       }
-    } 
-    else if (!loading) {
+    } else if (!loading) {
       content = (
-        <ScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        <View>
-          <Text style={styles.endText}>
-          Ihr habt alle Fragen beantwortet, Glückwunsch!
-          </Text>
-          <Text style={styles.endText}>
-          Eure erreichte Punktzahl: {points}
-          </Text>
-        </View>
-      </ScrollView>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View>
+            <Text style={styles.endText}>
+              Ihr habt alle Fragen beantwortet, Glückwunsch!
+            </Text>
+            <Text style={styles.endText}>
+              Eure erreichte Punktzahl: {points}
+            </Text>
+          </View>
+        </ScrollView>
       );
     }
   }
@@ -324,12 +395,12 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: 'center',
   },
-  refreshContainer:{ 
-    flexGrow: 1 
+  refreshContainer: {
+    flexGrow: 1,
   },
-  specialRefreshConatiner:{ 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
+  specialRefreshConatiner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
