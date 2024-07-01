85630e1895d87ee16c25f30c40415e09c252c049
Erik Behrends <behrends@users.noreply.github.com>
2024-07-01 16:22:34 +0200

    Format with Prettier

--- screens/questions/MultipleChoiceQuestions.jsx
+++ screens/questions/MultipleChoiceQuestions.jsx
@@ -1,43 +1,45 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
   View,
   Text,
-  TextInput,
   Button,
   Alert,
   StyleSheet,
   ScrollView,
   Platform,
-} from "react-native";
-import { useSetPoints } from "../../utils/Points";
-import { TouchableOpacity } from "react-native";
-import { useSharedStates } from "../../utils/SharedStates";
-import { supabase } from "../../utils/Supabase";
-import Constants from "../../utils/Constants";
-import Colors from "../../utils/Colors";
-import { confirmAlert } from "../../utils/ConfirmAlert";
-import HintComponent from "../../ui/HintComponent";
+} from 'react-native';
+import { useSetPoints } from '../../utils/Points';
+import { TouchableOpacity } from 'react-native';
+import { useSharedStates } from '../../utils/SharedStates';
+import Constants from '../../utils/Constants';
+import Colors from '../../utils/Colors';
+import { confirmAlert } from '../../utils/ConfirmAlert';
+import HintComponent from '../../ui/HintComponent';
 
 export default function MultipleChoiceQuestions() {
-  const [answer, setAnswer] = useState("");
-  const [confirmedAnswer, setConfirmedAnswer] = useState("");
+  const [answer, setAnswer] = useState('');
+  const [confirmedAnswer, setConfirmedAnswer] = useState('');
   const [answered, setAnswered] = useState(false);
   const { questions, currentQuestion, setCurrentQuestion, group } =
     useSharedStates();
   const setPoints = useSetPoints();
 
   const handleNext = async () => {
-    correctly_answered = answer.trim() === questions[currentQuestion].answer;
-    await setPoints(correctly_answered, questions[currentQuestion].points);
+    correctly_answered =
+      answer.trim() === questions[currentQuestion].answer;
+    await setPoints(
+      correctly_answered,
+      questions[currentQuestion].points
+    );
     setCurrentQuestion(currentQuestion + 1);
-    setAnswer("");
+    setAnswer('');
     setAnswered(false);
   };
 
   const handleAnswerSubmit = () => {
     setAnswered(true);
-    if (answer.trim() === "") {
-      Alert.alert("Fehler", "Bitte gebe eine Antwort ein.");
+    if (answer.trim() === '') {
+      Alert.alert('Fehler', 'Bitte gebe eine Antwort ein.');
       return;
     }
     confirmAlert(answer, handleNext);
@@ -49,31 +51,36 @@ export default function MultipleChoiceQuestions() {
         <Text style={styles.question}>
           {questions[currentQuestion].question}
         </Text>
-        {questions[currentQuestion].multiple_answer.map((option, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.squareButton}
-            onPress={() => setAnswer(option)}
-          >
-            <View
-              style={[
-                styles.innerSquare,
-                {
-                  backgroundColor: answer === option ? Colors.dhbwRed : "white",
-                },
-              ]}
-            />
-            <Text style={styles.answerText}>{option}</Text>
-          </TouchableOpacity>
-        ))}
+        {questions[currentQuestion].multiple_answer.map(
+          (option, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.squareButton}
+              onPress={() => setAnswer(option)}
+            >
+              <View
+                style={[
+                  styles.innerSquare,
+                  {
+                    backgroundColor:
+                      answer === option ? Colors.dhbwRed : 'white',
+                  },
+                ]}
+              />
+              <Text style={styles.answerText}>{option}</Text>
+            </TouchableOpacity>
+          )
+        )}
         <View
           style={
-            !answer ? styles.buttonContainerDeactive : styles.buttonContainer
+            !answer
+              ? styles.buttonContainerDeactive
+              : styles.buttonContainer
           }
         >
           <Button //Red Button
             style={styles.button}
-            color={Platform.OS === "ios" ? "white" : Colors.dhbwRed}
+            color={Platform.OS === 'ios' ? 'white' : Colors.dhbwRed}
             title="Antwort senden"
             onPress={handleAnswerSubmit}
             disabled={!answer}
@@ -82,7 +89,9 @@ export default function MultipleChoiceQuestions() {
 
         {confirmedAnswer ? (
           <View style={styles.answerContainer}>
-            <Text style={styles.answerLabel}>Bestätigte Antwort:</Text>
+            <Text style={styles.answerLabel}>
+              Bestätigte Antwort:
+            </Text>
             <Text style={styles.answer}>{confirmedAnswer}</Text>
           </View>
         ) : null}
@@ -94,9 +103,9 @@ export default function MultipleChoiceQuestions() {
 
 const styles = StyleSheet.create({
   squareButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     marginStart: 30,
     marginBottom: 20,
   },
@@ -112,26 +121,26 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flexGrow: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     paddingBottom: 200, // quickfix for keyboard covering input on small screens
   },
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 20,
   },
   question: {
     fontSize: 20,
     marginBottom: 30,
-    textAlign: "center",
+    textAlign: 'center',
   },
   inputLabel: {
     fontSize: 16,
     marginBottom: 5,
   },
   input: {
-    width: "100%",
+    width: '100%',
     height: 40,
     borderColor: Colors.dhbwGray,
     borderWidth: 1,
@@ -141,11 +150,11 @@ const styles = StyleSheet.create({
   },
   answerContainer: {
     marginTop: 20,
-    alignItems: "center",
+    alignItems: 'center',
   },
   answerLabel: {
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 5,
   },
   answer: {
