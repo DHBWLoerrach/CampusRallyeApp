853f152e7fe80374766bc84c91365c71cefeec05
Erik Behrends <behrends@users.noreply.github.com>
2024-11-06 16:54:45 +0100

    Remove unused stack screens from navigation

--- navigation/MainNavigator.jsx
+++ navigation/MainNavigator.jsx
@@ -3,8 +3,6 @@ import MainTabs from './MainTabs';
 import ImprintScreen from '../screens/ImprintScreen';
 import InformationScreen from '../screens/InformationScreen';
 import SkillQuestions from '../screens/questions/SkillQuestions';
-import UploadQuestions from '../screens/questions/UploadQuestions';
-import QRCodeQuestions from '../screens/questions/QRCodeQuestions';
 import Color from '../utils/Colors';
 
 const Stack = createNativeStackNavigator();
@@ -28,14 +26,6 @@ export default function MainNavigator() {
         component={InformationScreen}
       />
       <Stack.Screen name="Wissensfragen" component={SkillQuestions} />
-      <Stack.Screen
-        name="ImageQuestions"
-        component={UploadQuestions}
-      />
-      <Stack.Screen
-        name="QRCodeQuestions"
-        component={QRCodeQuestions}
-      />
     </Stack.Navigator>
   );
 }
