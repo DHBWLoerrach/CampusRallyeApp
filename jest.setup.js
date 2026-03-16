f145212cc43d6b860465ec23671b488ffe971ef8
Erik Behrends <behrends@users.noreply.github.com>
2026-03-16 17:30:31 +0100

    Stabilize geocaching test setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,36 @@
+jest.mock('react-native-worklets', () => ({
+  __esModule: true,
+  RuntimeKind: {
+    ReactNative: 'ReactNative',
+  },
+  WorkletsModule: {},
+  callMicrotasks: jest.fn(),
+  createSerializable: (value) => value,
+  createSynchronizable: (value) => value,
+  createWorkletRuntime: jest.fn(),
+  executeOnUIRuntimeSync: (fn) => fn,
+  getRuntimeKind: () => 'ReactNative',
+  getStaticFeatureFlag: () => false,
+  isSerializableRef: () => false,
+  isSynchronizable: () => false,
+  isWorkletFunction: () => false,
+  makeShareable: (value) => value,
+  makeShareableCloneOnUIRecursive: (value) => value,
+  makeShareableCloneRecursive: (value) => value,
+  runOnJS: (fn) => fn,
+  runOnRuntime: (fn) => fn,
+  runOnUI: (fn) => fn,
+  runOnUIAsync: (fn) => async (...args) => fn(...args),
+  runOnUISync: (fn) => fn(),
+  scheduleOnRN: (fn, ...args) => fn(...args),
+  scheduleOnRuntime: (fn, ...args) => fn(...args),
+  scheduleOnUI: (fn, ...args) => fn(...args),
+  serializableMappingCache: new Map(),
+  setDynamicFeatureFlag: jest.fn(),
+  shareableMappingCache: new Map(),
+  unstable_eventLoopTask: () => () => {},
+}));
+
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock')
 );
